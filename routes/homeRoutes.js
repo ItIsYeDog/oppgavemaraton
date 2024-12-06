d56d881cd83ca5711a36cd99e9ad9e43408ef1e0
itisyedogdev <itisyedogdev@gmail.com>
2024-12-06 14:11:59 +0100

    adding users panel/admin panel

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
+const isAdmin = require('../middleware/isAdmin');
+
 
 router.get('/', indexController.homePage);
-
 router.get('/helloworld', indexController.helloWorld);
+router.get('/users', isAdmin, indexController.listUsers);
+router.post('/users/:id/delete', isAdmin, indexController.deleteUser);
+router.post('/users/:id/upgrade', isAdmin, indexController.upgradeUser);
+
 
 module.exports = router;
