58be9f0bc60671f776cad5f0e86bd3ff5f828cfb
itisyedogdev <itisyedogdev@gmail.com>
2024-12-06 10:52:04 +0100

    adding

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
 
-
 router.get('/', indexController.homePage);
 
 router.get('/helloworld', indexController.helloWorld);
